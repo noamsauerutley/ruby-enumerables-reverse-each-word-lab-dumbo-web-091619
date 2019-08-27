26590583c70fc700b8d0fee93789d86f5c7de979
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-27 03:00:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   string_array = string.split(" ")
 
-  reversed_string = string_array.map!{|word| word.reverse}.join("")
+  reversed_string = string_array.map!{|word| word.reverse}.join(" ")
   return reversed_string
 end
